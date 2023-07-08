cc9a5c3f70b63bb1c710e9dbe89075e1a8c3e8fd
bryndalski <kubabryndal@gmail.com>
2023-07-08 15:54:42 +0200

    docker setup nestjs initial run

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new ConfigService();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ATS - admin ')
+    .setDescription('Admin swagger for ATS project')
+    .setVersion('1.1')
+    .addTag('Api', 'Admin')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app as any, swaggerConfig);
+  SwaggerModule.setup(
+    `${config.get('DATA_SERVER_PREFIX')}/docs`,
+    app as any,
+    document,
+  );
+  await app.listen(config.get('PORT'));
 }
 bootstrap();
